42d2bd45749a98aa13391027b403e95d31312370
Joshua Chuang <joshzngf@users.noreply.github.com>
2025-01-23 17:29:30 -0800

    Move aspect ratio to card

--- media-source-image-card.js
+++ media-source-image-card.js
@@ -13,6 +13,7 @@ class MediaSourceImageCard extends HTMLElement {
             ha-card {
               overflow: hidden;
               height: 100%;
+              aspect-ratio: ${this.config.aspect_ratio ? this.config.aspect_ratio : '1.9'};
               display: flex;
               align-content: center;
               justify-content: center;
@@ -24,8 +25,7 @@ class MediaSourceImageCard extends HTMLElement {
 
             img {
               display: block;
-              max-width: 100%;
-              aspect-ratio: ${this.config.aspect_ratio ? this.config.aspect_ratio : '1.9'};
+              width: 100%;
               object-fit: ${this.config.object_fit ? this.config.object_fit : 'contain'} ;
               object-position: ${this.config.object_position ? this.config.object_position : '50% 50%'} ;
             }
