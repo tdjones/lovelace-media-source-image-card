cbee3f5d1c46ddde2945a1589d6e01789b2b063a
Trevor Jones <trevor.jones.8@gmail.com>
2025-07-05 01:44:44 -0600

    Added a card editor interface
    * Can now select image via media selector

--- media-source-image-card.js
+++ media-source-image-card.js
@@ -1,9 +1,127 @@
+class MediaSourceImageCardEditor extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  setConfig(config) {
+    this._config = config;
+    this._render();
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+  }
+
+  _render() {
+    if (!this._hass || !this.shadowRoot) {
+      return;
+    }
+
+    const form = document.createElement("ha-form");
+    form.hass = this._hass;
+    form.data = this._config;
+    form.schema = [
+      {
+        name: "image",
+        required: true,
+        selector: {
+          media: {
+            accept: ["image/jpeg", "image/png", "image/gif"]
+          },
+        },
+      },
+      {
+        name: "entity_id",
+        selector: {
+          entity: {},
+        },
+      },
+      {
+        name: "apply_grayscale",
+        selector: {
+          boolean: {},
+        },
+      },
+      {
+        type: "grid",
+        name: "",
+        schema: [
+          { name: "aspect_ratio", selector: { text: {} } },
+          {
+            name: "object_fit",
+            selector: {
+              select: {
+                options: ["contain", "cover", "fill", "scale-down", "none"],
+              },
+            },
+          },
+        ],
+      },
+      { name: "object_position", selector: { text: {} } },
+      { name: "tap_action", selector: { "ui-action": {} } },
+      {
+        name: "forced_refresh_interval",
+        selector: { number: { min: 0, step: 1, mode: "box" } },
+      },
+    ];
+    form.computeLabel = this._computeLabel;
+    form.computeHelper = this._computeHelper;
+
+    form.addEventListener("value-changed", (ev) => {
+      const config = ev.detail.value;
+      const event = new CustomEvent("config-changed", {
+        detail: { config: config },
+        bubbles: true,
+        composed: true,
+      });
+      this.dispatchEvent(event);
+    });
+
+    this.shadowRoot.innerHTML = "";
+    this.shadowRoot.appendChild(form);
+  }
+
+  _computeLabel(schema) {
+    const labels = {
+      image: "Image",
+      entity_id: "Entity",
+      apply_grayscale: "Apply Grayscale on 'off' state",
+      aspect_ratio: "Aspect Ratio",
+      object_fit: "Object Fit",
+      object_position: "Object Position",
+      tap_action: "Tap Action",
+      forced_refresh_interval: "Forced Refresh Interval",
+    };
+    return labels[schema.name] || schema.name;
+  }
+
+  _computeHelper(schema) {
+    if (schema.name === "forced_refresh_interval") {
+      return "In seconds. Useful for images that are dynamically generated.";
+    }
+    return undefined;
+  }
+}
+
+customElements.define(
+  "media-source-image-card-editor",
+  MediaSourceImageCardEditor
+);
 class MediaSourceImageCard extends HTMLElement {
   static get properties() {
     return {
       hass: {},
       config: {}
     }
+  }
+
+  static async getConfigElement() {
+    return document.createElement("media-source-image-card-editor");
+  }
+
+  static getStubConfig() {
+    return { image: "media-source://" };
   }
 
   renderBase() {
@@ -83,11 +201,15 @@ class MediaSourceImageCard extends HTMLElement {
   }
 
   async getImageUrl(image) {
+    let imageUrl = image;
+    if (image && typeof image === "object") {
+      imageUrl = image.media_content_id;
+    }
     // if template, resolve rendered template:
-    if (this.config.image.indexOf('{{') > -1) return this.getMediaUrl(await this.renderTemplate(image));
-    if (this.config.image.indexOf('[[[') > -1) return this.getMediaUrl(await this.renderJsTemplate(image));
+    if (imageUrl && imageUrl.indexOf("{{") > -1) return this.getMediaUrl(await this.renderTemplate(imageUrl));
+    if (imageUrl && imageUrl.indexOf("[[[") > -1) return this.getMediaUrl(await this.renderJsTemplate(imageUrl));
     // else, call HA service to get media source url:
-    return this.getMediaUrl(image);
+    return this.getMediaUrl(imageUrl);
   }
 
   setConfig(config) {
@@ -99,6 +221,9 @@ class MediaSourceImageCard extends HTMLElement {
 
   watchEntities(input, hass) {
     if (!this.entitiesToWatch) this.entitiesToWatch = {};
+    if (typeof input !== "string") {
+        return false;
+    }
     let entities = [...input.matchAll(/[0-9a-zA-z]*\.[0-9a-zA-z]*/g)];
     let hasChanged = false;
     for (const entity of entities.map(e => e[0])) {
